c628f630886dbf1288657375750ba5eb097b91cb
Sarim Ahmed <88588593+thenoisyninga@users.noreply.github.com>
2024-02-25 11:23:53 +0500

    Added Question 1

--- Assignments/Assignment 1/A1-Q#1[23K0703].cpp
+++ Assignments/Assignment 1/A1-Q#1[23K0703].cpp
@@ -10,30 +10,31 @@ class Pet
     int numSpecialSkills;
 
 public:
-    Pet()
+    Pet() {}
+
+    Pet(int hungerLevel, int happinessLevel, string species, int numSpecialSkills)
     {
         // Input initial details
 
-        cout << "Enter Hunger Level: ";
-        cin >> hungerLevel;
+        this->hungerLevel = hungerLevel;
 
-        cout << "Enter Happiness Level: ";
-        cin >> happinessLevel;
+        this->happinessLevel = happinessLevel;
 
-        cout << "Enter Pet Species: ";
-        cin >> species;
+        this->species = species;
 
         updateHealth();
 
-        cout << "Enter Number Of Special Skills: ";
-        cin >> numSpecialSkills;
+        this->numSpecialSkills = numSpecialSkills;
 
-        specialSkills = new string[numSpecialSkills];
-
-        for (int i = 0; i < numSpecialSkills; i++)
+        if (this->numSpecialSkills > 0)
         {
-            cout << "Enter Skill " << i + 1 << ": ";
-            cin >> specialSkills[i];
+            specialSkills = new string[this->numSpecialSkills];
+
+            for (int i = 0; i < this->numSpecialSkills; i++)
+            {
+                cout << "Enter Skill " << i + 1 << ": ";
+                cin >> specialSkills[i];
+            }
         }
     }
 
@@ -74,7 +75,9 @@ public:
     void updateHunger()
     {
         string fedData;
-        cout << "Do you want to feed the pet? (Y/n): ";
+        fflush(stdin);
+        cout
+            << "Do you want to feed the pet? (Y/n): ";
         cin >> fedData;
 
         if (fedData == "Y" || fedData == "y")
@@ -106,10 +109,13 @@ public:
         cout << "Happiness Level: " << happinessLevel << endl;
         cout << "Hunger Level: " << hungerLevel << endl;
 
-        cout << "Special Skills: " << endl;
-        for (int i = 0; i < numSpecialSkills; i++)
+        if (this->numSpecialSkills > 0)
         {
-            cout << "Skill " << i + 1 << ": " << specialSkills[i] << endl;
+            cout << "Special Skills: " << endl;
+            for (int i = 0; i < this->numSpecialSkills; i++)
+            {
+                cout << "Skill " << i + 1 << ": " << specialSkills[i] << endl;
+            }
         }
         cout << endl;
     }
@@ -118,7 +124,7 @@ public:
 class Adopter
 {
     string name, mobileNumber;
-    Pet adoptedPets[10];
+    Pet *adoptedPets;
     int numberOfAdoptedPets = 0;
 
 public:
@@ -134,40 +140,51 @@ public:
     void adoptPet(Pet pet)
     {
         numberOfAdoptedPets++;
-        cout << "Creating new array" << endl;
         Pet *newAdoptedList = new Pet[numberOfAdoptedPets];
-        cout << "created new array" << endl;
 
-        for (int i = 0; i < numberOfAdoptedPets; i++)
+        for (int i = 0; i < numberOfAdoptedPets - 1; i++)
         {
             newAdoptedList[i] = adoptedPets[i];
         }
         newAdoptedList[numberOfAdoptedPets - 1] = pet;
 
+        Pet *temp = adoptedPets;
+        adoptedPets = newAdoptedList;
+        newAdoptedList = temp;
+
+        if (numberOfAdoptedPets - 1 > 0)
+        {
+            delete[] newAdoptedList;
+        }
     }
 
     void returnPet()
     {
         numberOfAdoptedPets--;
-        Pet *newAdoptedList = new Pet[numberOfAdoptedPets];
-
-        for (int i = 0; i < numberOfAdoptedPets; i++)
+        if (numberOfAdoptedPets > 0)
         {
-            newAdoptedList[i] = adoptedPets[i];
+            Pet *newAdoptedList = new Pet[numberOfAdoptedPets];
+
+            for (int i = 0; i < numberOfAdoptedPets; i++)
+            {
+                newAdoptedList[i] = adoptedPets[i];
+            }
+
+            Pet *temp = adoptedPets;
+            adoptedPets = newAdoptedList;
+            newAdoptedList = temp;
+
+            delete[] newAdoptedList;
         }
-
-        Pet *temp;
-        temp = adoptedPets;
-        adoptedPets = newAdoptedList;
-        newAdoptedList = temp;
-
-        delete[] newAdoptedList;
-        delete[] temp;
+        else
+        {
+            delete[] adoptedPets;
+        }
     }
 
     void displayAdoptedPets()
     {
-        for (int i = 0; i < numberOfAdoptedPets; i++)
+        for (int i = 0; i < this->numberOfAdoptedPets; i++)
         {
             adoptedPets[i].displayPetDetails();
         }
@@ -183,34 +200,74 @@ int main()
 
     cout << "[+] Demostrating the pet class..." << endl;
 
-    // Pet myPet1, myPet2;
-    // myPet1.displayPetDetails();
+    int happinessLevel, hungerLevel, numSpecialSkills;
+    string species;
 
-    // myPet1.updateHunger();
-    // myPet1.updateHealth();
-    // myPet1.updateHappiness();
-    // myPet1.displayPetDetails();
+    // Initializing pet one object
+    cout << "Enter happiness level for myPet1: ";
+    cin >> happinessLevel;
 
-    // myPet1.updateHunger();
-    // myPet1.updateHealth();
-    // myPet1.updateHappiness();
-    // myPet1.displayPetDetails();
+    cout << "Enter hunger level for myPet1: ";
+    cin >> hungerLevel;
 
-    // myPet1.updateHunger();
-    // myPet1.updateHealth();
-    // myPet1.updateHappiness();
-    // myPet1.displayPetDetails();
+    cout << "Enter species for myPet1: ";
+    cin >> species;
+
+    cout << "Enter number of myPet1's special skills: ";
+    cin >> numSpecialSkills;
+
+    Pet myPet1(hungerLevel, happinessLevel, species, numSpecialSkills);
+
+    myPet1.displayPetDetails();
+
+    // Initializing pet one object
+    cout << "Enter happiness level for myPet2: ";
+    cin >> happinessLevel;
+
+    cout << "Enter hunger level for myPet2: ";
+    cin >> hungerLevel;
+
+    cout << "Enter species for myPet2: ";
+    cin >> species;
+
+    cout << "Enter number of myPet2's special skills: ";
+    cin >> numSpecialSkills;
+
+    Pet myPet2(hungerLevel, happinessLevel, species, numSpecialSkills);
+
+    myPet2.displayPetDetails();
+
+    myPet1.updateHunger();
+    myPet1.updateHealth();
+    myPet1.updateHappiness();
+    myPet1.displayPetDetails();
+
+    myPet1.updateHunger();
+    myPet1.updateHealth();
+    myPet1.updateHappiness();
+    myPet1.displayPetDetails();
+
+    myPet1.updateHunger();
+    myPet1.updateHealth();
+    myPet1.updateHappiness();
+    myPet1.displayPetDetails();
 
     cout << "[+] Demostrating the adopter class..." << endl;
-    Pet myPet1;
 
     Adopter adopter;
 
+    cout << "\n[+] Adopting myPet1";
     adopter.adoptPet(myPet1);
-    // adopter.adoptPet(myPet2);
 
+    cout << "\n[+] Adopting myPet2";
+    adopter.adoptPet(myPet2);
+
+    cout << "\n[+] Displaying Adopted Pets";
     adopter.displayAdoptedPets();
 
+    cout << "\n[+] Returning Last Pet";
     adopter.returnPet();
+
+    cout << "\n[+] Displaying Adopted Pets";
     adopter.displayAdoptedPets();
 }
